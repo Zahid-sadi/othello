0a5ded09c30ee65de8d295fbc48cd2fc6d6343df
zahid-sadi <zahidbinkashem@gmail.com>
2024-12-12 14:26:12 +0600

    added shadow

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -9,7 +9,7 @@ export function Navbar() {
     return (
         <header
             className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 ${
-                scrolled ? "bg-white shadow-md" : "bg-transparent"
+                scrolled ? "bg-white shadow-lg shadow-gray-500" : "bg-transparent"
             }`}
         >
             <nav className="container mx-auto px-4 h-20 flex items-center justify-between">
